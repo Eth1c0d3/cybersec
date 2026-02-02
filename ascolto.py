41a42ee438c87959e0a74d47c0f8ac2fa5521b91
Eth1c0d3 <monica.fiocchi1@gmail.com>
2026-02-02 11:14:36 +0100

    Add files via upload

---
+++ ascolto.py
@@ -0,0 +1,29 @@
+#QUESTO SCRIPT VA AVVIATO PER PRIMO
+import socket
+server_socket = socket.socket()
+host = '127.0.0.1' #QUESTO E' IL MIO IP LOCALE
+port = 6364 #Questa è la porta che ho settato
+server_socket.bind((host, port))
+server_socket.listen(1)
+f = open('logfile.txt', 'w')
+f.write(f"Server listening on {host} : {port}\n")
+for i in range(6):
+    conn, addr = server_socket.accept() #accept è un medoto
+    f.write(f"Connected by {addr}\n")
+    try:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            richiesta = data.decode()
+            if richiesta == 'SHUTDOWN':
+                break
+            risposta = f"Ho ricevuto il tuo messaggio: {richiesta}\n"
+            f.write(risposta)
+            conn.sendall(risposta.encode())
+        if richiesta == 'SHUTDOWN':
+            break
+    finally:
+        conn.close()
+f.close()
+server_socket.close()
