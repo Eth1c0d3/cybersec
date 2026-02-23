1b64a585b146f9d1626733ba69d7da866f49c66e
Eth1c0d3 <monica.fiocchi1@gmail.com>
2026-02-23 11:41:21 +0100

    Update discount.py

--- discount.py
+++ discount.py
@@ -1,5 +1,20 @@
 def main():
-
+def discount(prezzi, animale, numero):
+    risultato = 0.0
+    base_sconto = 0.0
+    numero_animali = 0
+    if len(set(animale)) == 2: #verifico la presenza di articoli e animali
+        index = 0
+        for value in animale:
+            if value:
+                numero_animali = numero_animali + 1
+            else:
+                base_sconto = base_sconto + prezzi[index]
+            index = index + 1
+        if (numero - numero_animali) >= 5: #verifico la soglia 5 articoli
+            risultato = base_sconto * 0.2
+    return risultato
+    
 prices = []
 isPet = []
 sconto = 0.0
@@ -16,23 +31,6 @@ nItems = len(prices)
 if (nItems) == len(isPet):
     sconto = discount(prices, isPet, nItems)
 print(sconto)
-
-def discount(prezzi, animale, numero):
-    risultato = 0.0
-    base_sconto = 0.0
-    numero_animali = 0
-    if len(set(animale)) == 2: #verifico la presenza di articoli e animali
-        index = 0
-        for value in animale:
-            if value:
-                numero_animali = numero_animali + 1
-            else:
-                base_sconto = base_sconto + prezzi[index]
-            index = index + 1
-        if (numero - numero_animali) >= 5: #verifico la soglia 5 articoli
-            risultato = base_sconto * 0.2
-    return risultato
-
 
 if __name__ =="main":
   main()
