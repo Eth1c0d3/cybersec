f1dddc2f8037de99aa2606da82b12a9dc48c6755
Eth1c0d3 <monica.fiocchi1@gmail.com>
2026-02-23 11:49:27 +0100

    Update discount.py

--- discount.py
+++ discount.py
@@ -1,20 +1,15 @@
 def main():
-def discount(prezzi, animale, numero):
-    risultato = 0.0
-    base_sconto = 0.0
-    numero_animali = 0
-    if len(set(animale)) == 2: #verifico la presenza di articoli e animali
-        index = 0
-        for value in animale:
-            if value:
-                numero_animali = numero_animali + 1
-            else:
-                base_sconto = base_sconto + prezzi[index]
-            index = index + 1
-        if (numero - numero_animali) >= 5: #verifico la soglia 5 articoli
-            risultato = base_sconto * 0.2
-    return risultato
-    
+def dirOnly(istanza):
+    metodi = dir(istanza)
+    risultato = []
+    for m in metodi:
+        if not m.startwith('_'):
+            risultato.append(m)
+    return(risultato)
+f = open('lista_con_sconto.txt')
+with open('lista_con_sconto.txt', 'r') as file:
+    lines = file.readlines()
+    print(lines)
 prices = []
 isPet = []
 sconto = 0.0
@@ -30,8 +25,26 @@ with open('lista_con_sconto.txt', 'r') as file:
 nItems = len(prices)
 if (nItems) == len(isPet):
     sconto = discount(prices, isPet, nItems)
-print(sconto)
-
-if __name__ =="main":
-  main()
-  
+print(sconto)  
+pro_insieme = [False, False, False, True, False]
+index = 0
+for value in pro_insieme:
+    print(index, value)
+    index = index + 1
+    def discount(prezzi, animale, numero):
+    risultato = 0.0
+    base_sconto = 0.0
+    numero_animali = 0
+    if len(set(animale)) == 2: #verifico la presenza di articoli e animali
+        index = 0
+        for value in animale:
+            if value:
+                numero_animali = numero_animali + 1
+            else:
+                base_sconto = base_sconto + prezzi[index]
+            index = index + 1
+        if (numero - numero_animali) >= 5: #verifico la soglia 5 articoli
+            risultato = base_sconto * 0.2
+    return risultato
+if __name__ == "__main__":
+    main()
